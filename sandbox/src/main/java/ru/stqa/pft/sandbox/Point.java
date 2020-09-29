8dd186dd948619cb2432e267894cd6b18515b467
jynxxy <slawekpiesko@gmail.com>
2020-09-29 17:07:28 +0200

    task 2: calculating the distance between points - correcting the task

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,9 +3,9 @@ package ru.stqa.pft.sandbox;
 public class Point {
     double x, y;
 
-    public Point(double xcoord, double ycoord) {
-        this.x = xcoord;
-        this.y = ycoord;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
     public double getX() {
@@ -20,5 +20,5 @@ public class Point {
     public double distance(Point p1) {
         return Math.sqrt(Math.pow(x - p1.getX(), 2) + Math.pow(y - p1.getY(), 2));
     }
-    
+
 }
