db595b19696470dad8a5ca2a0fd3dc5d17a7189d
jynxxy <slawekpiesko@gmail.com>
2020-10-01 15:26:02 +0200

    creating class SquareTests and PointTests

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+import org.junit.Test;
+
+public class PointTests {
+
+    @Test
+    public void firstTest(){
+
+    }
+}
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class SquareTests {
+
+    @Test
+    public void testArea() {
+        Square s = new Square(5);
+        Assert.assertEquals(s.area(), 25);
+    }
+}
