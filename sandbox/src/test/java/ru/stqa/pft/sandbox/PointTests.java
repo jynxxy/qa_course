5ba5fd0aaafc5cb59f61038d8b798df6a35d4e41
jynxxy <slawekpiesko@gmail.com>
2020-10-01 16:09:19 +0200

    task 3: creating tests for Point class

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -6,14 +6,14 @@ import org.testng.Assert;
 public class PointTests {
 
     @Test
-    public void distanceTest1(){
+    public void falseDistanceTest(){
         Point p3 = new Point(5, 7);
         Point p4 = new Point(3,3);
         Assert.assertEquals(p3.distance(p4), 55);
     }
 
     @Test
-    public void distanceTest2(){
+    public void trueDistanceTest(){
         Point p5 = new Point(1,1);
         Point p6 = new Point(2,2);
         Assert.assertEquals(p5.distance(p6), 1.4142135623730951);
