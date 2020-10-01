952be38402a0386a277d43350dce6def2e93287b
jynxxy <slawekpiesko@gmail.com>
2020-10-01 15:53:53 +0200

    creating test for Point class

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -1,11 +1,21 @@
 package ru.stqa.pft.sandbox;
 
 import org.junit.Test;
+import org.testng.Assert;
 
 public class PointTests {
 
     @Test
-    public void firstTest(){
+    public void distanceTest1(){
+        Point p3 = new Point(5, 7);
+        Point p4 = new Point(3,3);
+        Assert.assertEquals(p3.distance(p4), 55);
+    }
 
+    @Test
+    public void distanceTest2(){
+        Point p5 = new Point(1,1);
+        Point p6 = new Point(2,2);
+        Assert.assertEquals(p5.distance(p6), 1.4142135623730951);
     }
 }
