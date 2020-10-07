00123f07b23bf604a7f7284a3529589eab91d6aa
jynxxy <slawekpiesko@gmail.com>
2020-10-07 13:35:05 +0200

    Create ContactHelper.java

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,40 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactHelper extends HelperBase {
+
+    public ContactHelper(FirefoxDriver wd) {
+        super(wd);
+    }
+
+    public void fillAddressBook(ContactData contactData) {
+        type(By.name("firstname"), contactData.getContactName());
+        type(By.name("lastname"), contactData.getContactSurname());
+        type(By.name("home"), contactData.getPhone());
+        type(By.name("email"), contactData.getEmail());
+    }
+
+    public void submitContactCreation() {
+        click(By.name("submit"));
+    }
+
+    public void selectContact() {
+        click(By.xpath("//input[@type='checkbox']"));
+    }
+
+    public void deleteContact() {
+        click(By.xpath("//input[@value='Delete']"));
+        wd.switchTo().alert().accept();
+    }
+
+    public void initContactModification() {
+        click(By.xpath("//img[@title='Edit']"));
+    }
+
+    public void submitContactModification() {
+        click(By.name("update"));
+    }
+}
