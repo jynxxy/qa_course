c32799f84d39487308394dc1d9857d5c77f908b4
jynxxy <slawekpiesko@gmail.com>
2020-10-11 13:02:57 +0200

    added Equation and EquationTests classes
    added Equation and EquationTests classes and corrected build.gradle

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -1,21 +1,21 @@
 package ru.stqa.pft.sandbox;
 
-import org.junit.Test;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class PointTests {
 
     @Test
-    public void falseDistanceTest(){
+    public void falseDistanceTest() {
         Point p3 = new Point(5, 7);
-        Point p4 = new Point(3,3);
+        Point p4 = new Point(3, 3);
         Assert.assertEquals(p3.distance(p4), 55);
     }
 
     @Test
-    public void trueDistanceTest(){
-        Point p5 = new Point(1,1);
-        Point p6 = new Point(2,2);
+    public void trueDistanceTest() {
+        Point p5 = new Point(1, 1);
+        Point p6 = new Point( 2, 2);
         Assert.assertEquals(p5.distance(p6), 1.4142135623730951);
     }
 }
