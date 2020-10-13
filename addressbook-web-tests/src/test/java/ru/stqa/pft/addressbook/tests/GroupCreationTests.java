637fc2f7db70ecab013733c1994e2be341c1121a
jynxxy <slawekpiesko@gmail.com>
2020-10-13 16:43:01 +0200

    checking and ensuring that the preconditions are fulfilled

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -8,10 +8,7 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().goToGroupPage();
-        app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
-        app.getGroupHelper().submitGroupCreation();
-        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
 
 }
