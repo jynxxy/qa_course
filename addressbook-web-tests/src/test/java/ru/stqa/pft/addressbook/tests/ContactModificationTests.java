c7581de3caad985955228b68bf8fa9bfb6e950e4
jynxxy <slawekpiesko@gmail.com>
2020-10-14 15:31:23 +0200

    working tasks 7 and 8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -8,7 +8,7 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactModification() {
         app.getContactHelper().initContactModification();
-        app.getContactHelper().fillContactForm(new ContactData("modname1", "modsurname1",
+        app.getContactHelper().fillContactForm(new ContactData("test1", "modsurname1",
                 "789", "modemail@email.com", null), false);
         app.getContactHelper().submitContactModification();
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase {
         app.getNavigationHelper().goToGroupPage();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("modtest1", "modtest2", "modtest3"));
+        app.getGroupHelper().fillGroupForm(new GroupData("modtest1", "modtest2", "modtest3"), false);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
     }
