03e9bc32231b087ca0c1275ce84445451c517c9d
jynxxy <slawekpiesko@gmail.com>
2020-10-18 14:28:37 +0200

    update testGroupCreation test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
-    public void testGroupCreation() throws Exception {
+    public void testGroupCreation() {
+        int before = app.getGroupHelper().getGroupCount();
         app.getNavigationHelper().goToGroupPage();
-        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        app.getGroupHelper().createGroup(new GroupData("test100", null, null));
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before +1);
     }
 
 }
