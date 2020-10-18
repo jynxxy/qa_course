d4aa9db04b3308bd2bf2eec54a4e39b37b81dfe1
jynxxy <slawekpiesko@gmail.com>
2020-10-18 15:26:20 +0200

    correction of testGroupCreation method

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -8,11 +8,11 @@ public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
-        int before = app.getGroupHelper().getGroupCount();
         app.getNavigationHelper().goToGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().createGroup(new GroupData("test100", null, null));
         int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before +1);
+        Assert.assertEquals(after, before + 1);
     }
 
 }
