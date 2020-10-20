ee5f6ec6dda1d2b524fa193724d3e91804a95d97
jynxxy <slawekpiesko@gmail.com>
2020-10-20 16:23:45 +0200

    collection comparison for contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook.model;
 
 public class ContactData {
-    private int id;
     private final String contactname;
     private final String contactsurname;
     private final String phone;
@@ -16,14 +15,31 @@ public class ContactData {
         this.group = group;
     }
 
-//    public ContactData(int id, String contactname, String contactsurname, String phone, String email, String group) {
-//        this.id = id;
-//        this.contactname = contactname;
-//        this.contactsurname. contactsurname;
-//        this.phone = phone;
-//        this.email = email;
-//        this.group = group;
-//    }
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "contactname='" + contactname + '\'' +
+                ", contactsurname='" + contactsurname + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactData that = (ContactData) o;
+
+        if (contactname != null ? !contactname.equals(that.contactname) : that.contactname != null) return false;
+        return contactsurname != null ? contactsurname.equals(that.contactsurname) : that.contactsurname == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = contactname != null ? contactname.hashCode() : 0;
+        result = 31 * result + (contactsurname != null ? contactsurname.hashCode() : 0);
+        return result;
+    }
 
     public String getContactName() {
         return contactname;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -20,6 +20,9 @@ public class ContactDeletionTests extends TestBase {
         app.getContactHelper().returnToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(after, before);
     }
 
 }
