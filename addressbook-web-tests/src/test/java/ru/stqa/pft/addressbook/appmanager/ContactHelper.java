5852c55c6b20dbb72190d0103070f0b0b88a97b2
jynxxy <slawekpiesko@gmail.com>
2020-10-27 15:24:18 +0100

    gluing chains

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -92,9 +92,9 @@ public class ContactHelper extends HelperBase {
             int id = Integer.parseInt(row.findElement(By.tagName("input")).getAttribute("value"));
             String firstname = cells.get(2).getText();
             String lastname = cells.get(1).getText();
-            String[] phones = cells.get(5).getText().split("\n");
+            String allPhones = cells.get(5).getText();
             contacts.add(new ContactData().withId(id).withFirstName(firstname).withLastName(lastname).
-                    withHomePhone(phones[0]).withMobilePhone(phones[1]).withWorkPhone(phones[2]));
+                    withAllPhones(allPhones));
         }
         return contacts;
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -9,6 +9,7 @@ public class ContactData {
     private String work;
     private String email;
     private String group;
+    private String allPhones;
 
     @Override
     public String toString() {
@@ -69,6 +70,15 @@ public class ContactData {
 
     public String getGroup() {
         return group;
+    }
+
+    public String getAllPhones() {
+        return allPhones;
+    }
+
+    public ContactData withAllPhones(String allPhones) {
+        this.allPhones = allPhones;
+        return this;
     }
 
     public ContactData withId(int id) {
