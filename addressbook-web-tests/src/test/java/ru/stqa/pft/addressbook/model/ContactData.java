d2ddc710fccba12a4f75642b4fdb0f77f4c68634
jynxxy <slawekpiesko@gmail.com>
2020-11-02 13:32:06 +0100

    Paths to files and directories - groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.io.File;
+
 public class ContactData {
     private int id = Integer.MAX_VALUE;
     private String firstname;
@@ -14,6 +16,8 @@ public class ContactData {
     private String email3;
     private String group;
     private String allPhones;
+    private File photo;
+
 
     @Override
     public String toString() {
@@ -96,6 +100,10 @@ public class ContactData {
         return allPhones;
     }
 
+    public File getPhoto() {
+        return photo;
+    }
+
     public ContactData withAllPhones(String allPhones) {
         this.allPhones = allPhones;
         return this;
@@ -161,4 +169,8 @@ public class ContactData {
         return this;
     }
 
+    public ContactData withPhoto(File photo) {
+        this.photo = photo;
+        return this;
+    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -12,9 +14,10 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreation() {
         Contacts before = app.contact().all();
-        ContactData contact = new ContactData().
-                withFirstName("name3").withLastName("surname3").
-                withHomePhone("444").withEmail("test@test.com").withGroup("test1");
+        File photo = new File("src/test/resources/stru.jpg");
+        ContactData contact = new ContactData()
+                .withFirstName("name3").withLastName("surname3").withPhoto(photo)
+                .withHomePhone("444").withEmail("test@test.com").withGroup("test1");
         app.goTo().AddNewContact();
         app.contact().create(contact);
         Contacts after = app.contact().all();
@@ -23,4 +26,12 @@ public class ContactCreationTests extends TestBase {
                 before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
     }
 
+    @Test
+    public void testCurrentDir() {
+        File currentDir = new File(".");
+        System.out.println(currentDir.getAbsolutePath());
+        File photo = new File("src/test/resources/stru.jpg");
+        System.out.println(photo.getAbsolutePath());
+        System.out.println(photo.exists());
+    }
 }
