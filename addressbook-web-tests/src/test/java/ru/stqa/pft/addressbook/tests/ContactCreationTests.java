c3dae49cf88bead3f772790becf9a1dacc960ed2
jynxxy <slawekpiesko@gmail.com>
2020-11-03 13:59:44 +0100

    Parameterization of test methods-contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,23 +1,37 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContacts(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new ContactData().withFirstName("firstname1").withLastName("lastname1")
+                .withHomePhone("987").withEmail("email1@test.com").withGroup("test 1")});
+        list.add(new Object[] {new ContactData().withFirstName("firstname1").withLastName("lastname2")
+                .withHomePhone("654").withEmail("email2@test.com").withGroup("test 1")});
+        list.add(new Object[] {new ContactData().withFirstName("firstname3").withLastName("lastname3")
+                .withHomePhone("321").withEmail("email3@test.com").withGroup("test 1")});
+        return list.iterator();
+    }
+
+    @Test (dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) {
+
         Contacts before = app.contact().all();
         File photo = new File("src/test/resources/stru.jpg");
-        ContactData contact = new ContactData()
-                .withFirstName("name3").withLastName("surname3").withPhoto(photo)
-                .withHomePhone("444").withEmail("test@test.com").withGroup("test1");
         app.goTo().AddNewContact();
         app.contact().create(contact);
         Contacts after = app.contact().all();
