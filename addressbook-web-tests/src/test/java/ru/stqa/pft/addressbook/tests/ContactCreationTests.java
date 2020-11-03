556e1ab12197a9fcbb324682fae3a496e852f0ef
jynxxy <slawekpiesko@gmail.com>
2020-11-03 14:29:56 +0100

    Loading test data from a file-contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,7 +5,10 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,20 +19,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts(){
+    public Iterator<Object[]> validContacts() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[] {new ContactData().withFirstName("firstname1").withLastName("lastname1")
-                .withHomePhone("987").withEmail("email1@test.com").withGroup("test 1")});
-        list.add(new Object[] {new ContactData().withFirstName("firstname1").withLastName("lastname2")
-                .withHomePhone("654").withEmail("email2@test.com").withGroup("test 1")});
-        list.add(new Object[] {new ContactData().withFirstName("firstname3").withLastName("lastname3")
-                .withHomePhone("321").withEmail("email3@test.com").withGroup("test 1")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[]{new ContactData().withFirstName(split[0]).withLastName(split[1])
+                    .withHomePhone(split[2]).withEmail(split[3])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
-    @Test (dataProvider = "validContacts")
+    @Test(dataProvider = "validContacts")
     public void testContactCreation(ContactData contact) {
-
         Contacts before = app.contact().all();
         File photo = new File("src/test/resources/stru.jpg");
         app.goTo().AddNewContact();
