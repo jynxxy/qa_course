9782e3029f0fe79bae9d8cf9789111f65bff22e9
jynxxy <slawekpiesko@gmail.com>
2020-11-07 09:55:29 +0100

    update 9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDetailsTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDetailsTest.java
@@ -2,15 +2,12 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
-import org.junit.Assert;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-import java.time.LocalDate;
 import java.time.Year;
-import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class ContactDetailsTest extends TestBase {
@@ -33,14 +30,16 @@ public class ContactDetailsTest extends TestBase {
     }
 
     private String getAll(ContactData contact) {
-        return  Arrays.asList(paragraph_1(contact), paragraph_2(contact), paragraph_3(contact),
-                paragraph_4(contact), paragraph_5(contact))
-                .stream().filter((s -> !s.equals("")))
+       List <String>  dupa =
+        Arrays.asList(paragraph_1(contact), paragraph_2(contact), paragraph_3(contact),
+                paragraph_4(contact), paragraph_5(contact));
+            String kupa = dupa.stream().filter((s -> !s.equals("")))
                 .map(ContactDetailsTest::clean)
                 .collect(Collectors.joining(""));
+            return kupa;
     }
 
-    public static String clean (String cleanedDetails) {
+    public static String clean(String cleanedDetails) {
         return cleanedDetails.replaceAll("\n", "");
     }
 
@@ -52,49 +51,54 @@ public class ContactDetailsTest extends TestBase {
         return s != null && !"".equals(s);
     }
 
-    private <T> String paragraph_1(ContactData contact) {
+    private String paragraph_1(ContactData contact) {
         return Arrays.asList(contact.getFirstName(), contact.getMiddlename(), contact.getLastName())
                 .stream().filter(this::isNotEmpty)
                 .collect(Collectors.joining(" "));
     }
 
     private String paragraph_2(ContactData contact) {
-        return  Arrays.asList(contact.getNick(), contact.getTitle(), contact.getCompany(), contact.getAddress())
+        return Arrays.asList(contact.getNick(), contact.getTitle(), contact.getCompany(), contact.getAddress())
                 .stream().filter(this::isNotEmpty)
                 .collect(Collectors.joining("\n"));
     }
 
-    private <T> String paragraph_3(ContactData contact) {
+    private String paragraph_3(ContactData contact) {
         return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone(), contact.getFax())
                 .stream().filter(this::isNotEmpty)
                 .collect(Collectors.joining("\n"));
     }
 
-    private <T> String paragraph_4(ContactData contact) {
+    private String paragraph_4(ContactData contact) {
         return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3(),
-                contact.getHomepage(),contact.getAddress2(), contact.getPhone2(), contact.getNotes())
+                contact.getHomepage(), contact.getAddress2(), contact.getPhone2(), contact.getNotes())
                 .stream().filter(this::isNotEmpty)
                 .map(ContactDetailsTest::cleanDomain)
                 .collect(Collectors.joining("\n"));
     }
 
-    private <T> String paragraph_5(ContactData contact) {
+    private String paragraph_5(ContactData contact) {
+        String year = contact.getBirthday_year();
         return Arrays.asList(" " + contact.getBirthday_day(), contact.getBirthday_month(),
-                contact.getBirthday_year(), birthday(contact))
+                year, countAge(year))
                 .stream().filter(this::isNotEmpty)
                 .map(ContactDetailsTest::cleanDate)
                 .collect(Collectors.joining(" "));
     }
 
-    private String birthday(ContactData contact) {
-        String year = contact.getBirthday_year();
-        int yearInt = Integer.parseInt(year);
-        int actualYear = Year.now().getValue();
-        int actualAgeInt = actualYear - yearInt;
-        String actualAge = Integer.toString(actualAgeInt);
 
+
+    private String countAge(String year) {
+        String actualAge = "";
+        if (year != null && !year.equals("")) {
+            int yearInt = Integer.parseInt(year);
+            int actualYear = Year.now().getValue();
+            int actualAgeInt = actualYear - yearInt;
+            actualAge = Integer.toString(actualAgeInt);
+        }
         return actualAge;
     }
+
 
     public static String cleanDate(String cleanDate) {
         return cleanDate.replaceAll("-", "");
