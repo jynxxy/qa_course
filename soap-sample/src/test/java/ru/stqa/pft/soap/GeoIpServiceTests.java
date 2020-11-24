1c49f400b71cb012e16e3995ef6c903beda5e230
jynxxy <slawekpiesko@gmail.com>
2020-11-24 13:51:08 +0100

    SOAP: a simple example_2

--- soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.soap;
 
 import com.lavasoft.GeoIPService;
+import com.lavasoft.GeoIPServiceHttpGet;
+import com.lavasoft.GeoIPServiceSoap;
 import org.hamcrest.MatcherAssert;
 import org.testng.annotations.Test;
 
@@ -10,7 +12,13 @@ public class GeoIpServiceTests {
 
     @Test
     public void testMyIp() {
-        String geoIP = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("194.28.29.152");
-        assertEquals(geoIP, "RUS");
+        String geoIP = new GeoIPService().getGeoIPServiceSoap12().getIpLocation20("194.28.29.152");
+        assertEquals(geoIP, "<GeoIP><Country>RU</Country><State>47</State></GeoIP>");
+    }
+
+    @Test
+    public void testInvalidIP() {
+        String geoIP = new GeoIPService().getGeoIPServiceSoap12().getIpLocation20("194.28.29.xxx");
+        assertEquals(geoIP, "<GeoIP><Country>RU</Country><State>47</State></GeoIP>");
     }
 }
