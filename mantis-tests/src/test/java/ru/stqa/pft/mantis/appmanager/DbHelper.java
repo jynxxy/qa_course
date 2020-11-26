9f359ba6600a046a88f1b70922f0f1fbdb437088
jynxxy <slawekpiesko@gmail.com>
2020-11-26 17:02:34 +0100

    update_1

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/DbHelper.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.hibernate.SessionFactory;
+
+public class DbHelper {
+
+    private final SessionFactory sessionFactory;
+
+    public DbHelper() {
+
+    }
+
+
+}
